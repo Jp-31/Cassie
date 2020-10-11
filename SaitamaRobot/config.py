3ba9b643a58d6e3057fbb77c49a70d0a37b080a1
Jp-31 <72206460+Jp-31@users.noreply.github.com>
2020-10-11 23:43:04 +0800

    Create config.py

---
+++ SaitamaRobot/config.py
@@ -0,0 +1,12 @@
+from SaitamaRobot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 254318997  # your telegram ID
+    OWNER_USERNAME = "Jeeeypi"  # your telegram username
+    API_KEY = "your bot api key"  # your api key, as provided by the @botfather
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
+    JOIN_LOGGER = '-1234567890' # some group chat that your bot is a member of
+    USE_JOIN_LOGGER = True
+    SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
+    LOAD = []
+    NO_LOAD = ['translation']
